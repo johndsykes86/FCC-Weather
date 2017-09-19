6fbfc1ed51acd45a8b1f69007d6e33852a12ada1
Johnathon Sykes <hireme@mrjsykes.com>
2017-09-19 13:49:30 -0700

    Added and tested API call to script.js

--- JS/script.js
+++ JS/script.js
@@ -1,8 +1,24 @@
 if ("geolocation" in navigator){
   navigator.geolocation.getCurrentPosition(function(position){
-    console.log(position)
+    const { latitude } = position.coords
+    const { longitude } = position.coords
+    // const url = `https://fcc-weather-api.glitch.me/api/current?lat=${latitude}&lon=${longitude}`
+    const url = `https://fcc-weather-api.glitch.me/api/current?lat=${latitude}&lon=${longitude}`
+
+    var myHeaders = new Headers()
+    var myInit = {method: 'GET',
+                  headers: myHeaders,
+                  mode: 'cors',
+                  credentials: 'include'}
+
+    fetch(url)
+     .then(res => res.json())
+       .then((data)=>{
+         console.log(data)
+       }).catch(err=>console.log(error))
+
+
   })
-}
-else {
+} else {
   console.log("Geolocation failed")
 }
